b9ad4a191ec02f31b83e159407bc44ad53df07f5
churabou <ginga7888@gmail.com>
2018-04-11 00:38:14 +0900

    why what

--- MyQiita/Config.swift
+++ MyQiita/Config.swift
@@ -1,1 +1,28 @@
-    static let clientId = "fb06d5b520b4473dc0cf5b122b31fa4d34fdbb46"
+//
+//  Config.swift
+//  MyQiita
+//
+//  Created by ちゅーたつ on 2018/04/08.
+//  Copyright © 2018年 ちゅーたつ. All rights reserved.
+//
+
+import Foundation
+
+struct Config {
+    static let clientId = ""
+    static let secret = ""
+    static var token: String {
+        return UserDefaults.standard.string(forKey: "access_toeken_key") ?? ""
+    }
+    
+    static var user: User? {
+        guard let data = UserDefaults.standard.data(forKey: "auto_memories_doll") else {
+            return nil
+        }
+        let user = try? JSONDecoder().decode(User.self, from: data)
+        return user
+    }
+}
+
+
+
